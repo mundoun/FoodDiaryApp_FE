12c02664d34407b9bcb65c60a825d00bb113cdae
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-12-27 11:46:05 +0900

    Fix : 불필요한 commom 패키지 삭제

--- FoodDiaryApp/core/model/src/main/kotlin/com/example/fooddiaryapp/model/common/DietError.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fooddiaryapp.model.common
-
-// 에러 타입 정의
-
-data class DietError(
-    val code: String,
-    val message: String
-)
--- FoodDiaryApp/core/model/src/main/kotlin/com/example/fooddiaryapp/model/common/Result.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fooddiaryapp.model.common
-
-// 데이터 결과 래퍼 클래스
-
-sealed interface Result<out T> {
-    data class Success<T>(val data: T) : Result<T>
-    data class Error(val error: DietError) : Result<Nothing>
-    object Loading : Result<Nothing>
-}
